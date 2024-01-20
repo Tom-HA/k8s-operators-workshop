d4d1e4f6dad5017d7814b976e4cabd7c35ad2ea7
Tom <33584517+Tom-HA@users.noreply.github.com>
2024-01-21 01:12:09 +0200

    Update sample app (#8)
    Refactor sample-pg-app and CI

--- sample-pg-app/main.go
+++ sample-pg-app/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"sync"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -24,14 +22,12 @@ var (
 	pgPort      = os.Getenv("POSTGRES_PORT")
 	pgUser      = os.Getenv("POSTGRES_USER")
 	pgPassword  = os.Getenv("POSTGRES_PASSWORD")
-	dbName      = os.Getenv("POSTGRES_DB")
+	pgDBName    = os.Getenv("POSTGRES_DB")
+	pgSSLMode   = os.Getenv("POSTGRES_SSLMODE")
 	servicePort = os.Getenv("SERVICE_PORT")
-
-	DBConn *sql.DB
-	lock   = sync.Mutex{}
 )
 
-type PostgresConnection struct {
+type PostgresConConfig struct {
 	host     string
 	port     string
 	user     string
@@ -44,60 +40,72 @@ type TableData struct {
 	Data string `json:"data"`
 }
 
-func init() {
-	fmt.Println(os.Environ())
-	pgConnection := PostgresConnection{
+func main() {
+
+	if pgSSLMode == "" {
+		pgSSLMode = "disable"
+	}
+
+	dbConn, err := initDatabase(PostgresConConfig{
 		host:     pgHost,
 		port:     pgPort,
 		user:     pgUser,
 		password: pgPassword,
-		dbName:   dbName,
-		sslMode:  "disable",
-	}
-
-	var err error
-	DBConn, err = getDBConnection(pgConnection)
+		dbName:   pgDBName,
+		sslMode:  pgSSLMode,
+	})
 	if err != nil {
 		panic(err)
 	}
+	defer dbConn.Close()
 
-	err = pingDatabase(DBConn)
-	if err != nil {
-		panic(err)
-	}
-
-	err = initDatabase(DBConn)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func main() {
-	defer DBConn.Close()
 	e := echo.New()
-
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/health"
+		},
+	}))
 	e.Use(middleware.Recover())
 
 	e.GET("/health", func(c echo.Context) error {
+		err := dbConn.Ping()
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("could not connect to the database: %w", err))
+		}
 		return c.NoContent(http.StatusOK)
 	})
 	e.GET("/data", func(c echo.Context) error {
-		return getData(c, DBConn)
+		return getData(c, dbConn)
 	})
 	e.POST("/data", func(c echo.Context) error {
-		return addData(c, DBConn)
+		return addData(c, dbConn)
 	})
 
 	if servicePort == "" {
 		servicePort = "5001"
 	}
-
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", servicePort)))
-
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", servicePort)))
 }
 
-func getDBConnection(connectionInfo PostgresConnection) (dbConnection *sql.DB, err error) {
+func initDatabase(connectionInfo PostgresConConfig) (dbConnection *sql.DB, err error) {
+	dbConn, err := getDBConnection(connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = dbConn.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to the database: %w", err)
+	}
+
+	err = provisionDatabase(dbConn)
+	if err != nil {
+		return nil, err
+	}
+	return dbConn, nil
+}
+
+func getDBConnection(connectionInfo PostgresConConfig) (dbConnection *sql.DB, err error) {
 	pgConnectionInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=%s",
 		connectionInfo.host, connectionInfo.port, connectionInfo.user,
@@ -110,23 +118,7 @@ func getDBConnection(connectionInfo PostgresConnection) (dbConnection *sql.DB, e
 	return db, nil
 }
 
-func pingDatabase(dbConnection *sql.DB) error {
-	waitSeconds := 5
-	for counter := 0; counter < 7; counter++ {
-		err := dbConnection.Ping()
-		if counter == 6 {
-			return fmt.Errorf("exceeded max timeout %d, could not connect to the database: %w", waitSeconds*6, err)
-		}
-		if err != nil {
-			time.Sleep(time.Duration(waitSeconds) * time.Second)
-		}
-	}
-
-	fmt.Println("Successfully connected to the database!")
-	return nil
-}
-
-func initDatabase(dbConnection *sql.DB) error {
+func provisionDatabase(dbConnection *sql.DB) error {
 	query := `
 	CREATE TABLE IF NOT EXISTS %[1]s (
 		%[2]s varchar(45) NOT NULL,
@@ -137,55 +129,4 @@ func initDatabase(dbConnection *sql.DB) error {
 	_, err := dbConnection.Exec(createTableQuery)
 
 	return err
-}
-
-func addData(c echo.Context, dbConnection *sql.DB) error {
-	lock.Lock()
-	defer lock.Unlock()
-
-	var data TableData
-	err := c.Bind(&data)
-	if err != nil {
-		return err
-	}
-
-	if data.Data == "" {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Data cannot be empty"})
-	}
-
-	query := `
-	insert into "%s"("%s") values($1) 
-	`
-	insertDataQuery := fmt.Sprintf(query, TableName, ColumnName)
-	_, err = dbConnection.Exec(insertDataQuery, data.Data)
-
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusCreated, data)
-}
-
-func getData(c echo.Context, dbConnection *sql.DB) error {
-	lock.Lock()
-	defer lock.Unlock()
-
-	query := `
-	select * from "%s"
-	`
-	getDataQuery := fmt.Sprintf(query, TableName)
-	rows, err := dbConnection.Query(getDataQuery)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
-	var dataResponse []TableData
-	for rows.Next() {
-		var data TableData
-		rows.Scan(&data.Data)
-		dataResponse = append(dataResponse, data)
-	}
-
-	return c.JSON(http.StatusOK, dataResponse)
 }
